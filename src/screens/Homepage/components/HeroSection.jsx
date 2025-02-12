d7894bfdce8dd7201ac655bf6fe05b13e29acecd
Yashnotfound <userongrid@Admins-MacBook-Pro-8.local>
2025-02-12 18:16:58 +0530

    refactored document create code

--- src/screens/Homepage/components/HeroSection.jsx
+++ src/screens/Homepage/components/HeroSection.jsx
@@ -4,11 +4,13 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-
 import React from "react";
 import CardHolder from "./CardContainer";
+import { UserContext } from "../../../App";
+import {useContext} from "react";
 
-const HeroSection = ({value, changeHandler, isAuthenticated}) => {
+const HeroSection = ({value, changeHandler}) => {
+    const {userAuth:{accessToken} = {}} = useContext(UserContext);
   return (
     <Container>
         <TabContext value={value}>
@@ -16,7 +18,7 @@ const HeroSection = ({value, changeHandler, isAuthenticated}) => {
             <TabList onChange={changeHandler} aria-label="lab API tabs example" centered>
             <Tab label="General Docs" value="general" />
             <Tab label="API Docs" value="api" />
-            {isAuthenticated&<Tab label="Your Docs" value="user" />}
+            {accessToken?<Tab label="Your Docs" value="user" />:null}
             </TabList>
         </Box>
         <TabPanel value="general">{<CardHolder />}</TabPanel>
